e4bca43d304f339943348cd2335fbb3154340738
wenlai <wenlai_zhou@126.com>
2020-09-28 15:00:17 +0800

    flask-restx=0.2.0 版本有问题

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     zip_safe=True,
     install_requires=[
         'requests',
+        'flask-restplus',
         'flask-restx==0.1.1',
         'flask-cors==3.0.9',
         'Pillow==7.2.0',
